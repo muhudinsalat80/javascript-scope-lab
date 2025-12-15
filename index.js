f96e9ca57c92f728843c01ec4955ffbf662f57a2
muhudinsalat <muhudinsalat80@gmail.com>
2025-12-15 08:14:30 +0100

    the flatburger menu

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+//  GLOBAL SCOPE
+const burgers = ["Hamburger", "Cheeseburger"];
+
+let featuredDrink = "Strawberry Milkshake";
+
+// FUNCTION SCOPE
+function addBurger() {
+  const newBurger = "Flatburger";
+  burgers.push(newBurger);
+}
+
+// BLOCK SCOPE
+if(true){
+  const anotherNewBurger = "Maple Bacon Burger";
+  burgers.push(anotherNewBurger);
+}
+
+
+//  FUNCTION TO CHANGE DRINK
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+}
+
+
+
